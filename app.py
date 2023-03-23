dce99ef2d56cfbdaeaabaeec9e2326dd08deb5a9
Federico Soria <fsoria7@outlook.com>
2023-03-23 14:12:27 +0100

    make task 20

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from locations import Locations
+from forms import AddLocationForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'SECRET_PROJECT'
@@ -22,19 +23,20 @@ def locations(category):
     elif action == DEL_ACTION:
       visit.delete(name)
   ## Return the main template with variables
-  return render_template('locations.html', category=category, categories=categories, locations=locations)
+  return render_template('locations.html', category=category, categories=categories, locations=locations, add_location=AddLocationForm())
 
 @app.route("/add_location", methods=["POST"])
 def add_location():
   ## Validate and collect the form data
 
-  if True:
-      name=None
-      description=None
-      category=None
+  if validate_on_submit():
+      name=add_form.name.data
+      description=add_form.description.data
+      category=add_form.category.data
       visit.add(name, description, category)
 
   ## Redirect to locations route function
+  AddLocationForm(csrf_enabled=False)
   return ""
 
 @app.route("/")
